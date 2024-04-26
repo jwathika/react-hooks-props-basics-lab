51c5789f2fdcfa229a4259d486dbc6a5d1fa8971
jwathika <wathika02@gmail.com>
2024-04-26 12:48:39 +0300

    props&&conditional

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
-import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
+import React from 'react';
+import NavBar from './NavBar';
+import Home from './Home';
+import About from './About';
 
 // pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
+import user from '../data/user';
 console.log(user);
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+	return (
+		<div>
+			<NavBar />
+			<Home name={user.name} city={user.city} color={user.color} />
+			<About
+				bio={user.bio}
+				linkedin={user.links.linkedin}
+				github={user.links.github}
+			/>
+		</div>
+	);
 }
 
 export default App;
